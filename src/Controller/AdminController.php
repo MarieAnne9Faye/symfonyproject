444f47fb357240035b831a8c76bedca96ec3878f
Mamadou NDAO [SNT DST/DD/DPD/PMA] <Mamadou.NDAO@orange-sonatel.com>
2022-06-10 23:43:58 +0000

    my hotel

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,21 +2,24 @@
 
 namespace App\Controller;
 
-
+use App\Entity\Users;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Mapping\Entity;
 use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
+use PhpParser\Builder\Use_;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class MainController extends AbstractController
+class AdminController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/admin', name: 'app_bo')]
     public function index(): Response
     {
-        return $this->render('main/accueil.html.twig', [
+        return $this->render('back_office/panel.html.twig', [
+            'controller_name' => 'MainController',
         ]);
     }   
 }
